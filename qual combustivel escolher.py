a0111c81c2c1835294f38ffe77a2e39f05e1955f
EKolling <82099922+EKolling@users.noreply.github.com>
2022-05-24 15:39:11 -0300

    add initial template

---
+++ qual combustivel escolher.py
@@ -0,0 +1,11 @@
+from struct import calcsize
+
+
+Valor_alcool = input('qual o valor do alcool?')
+Valor_gasolina = input('qual o valor da gasolina?')
+capacidade_tanque = input(" qual a capacidade do seu tanque?")
+#media_g = input('quantos kml seu carro faz com a gasolina?')
+#media_a = input('quantos kml seu carro faz com alcool?')
+
+print (" se abastecer com alcool, ira gastar " Valor_alcool*capacidade_tanque)
+print (" se abastecer com gasolina ira gastar " Valor_gasolina* capacidade_tanque)
